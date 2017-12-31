e63a850604d27c886d79d23f8bea7cf2b1a18469
Unknown <joaquincora@hotmail.com>
2017-12-31 02:03:44 -0600

    comentarios
    Se comenta el test del crud para un mejor entendimiento.

--- src/test/java/net/tecgurus/hibernate/models/Test.java
+++ src/test/java/net/tecgurus/hibernate/models/Test.java
@@ -18,48 +18,72 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		
-		
+		//Se crea una sesión con hibernate.
 		Session session = HibernateUtil.getSessionFactory().openSession();
+		//Se inicia una transacción. 
 		session.beginTransaction();
 		
 		//Crear objeto de Maestro.
 		Maestro objMaestro = new Maestro(24,"nuevo");
-		objMaestro.setNombre("Jesica");  //insert into maestro (nombre) values (?)
 		
-		//Salvar el maestro.
+		//Se ejecuta un INSERT.
 		//session.save(objMaestro);
+		
+		//Se ejecuta un DELETE.
 		//session.delete(objMaestro);
+		
+		//Se ejecuta un UPDATE.
 		//session.update(objMaestro);
 		
+		
+		//------------------
+		// #### SELECT ####
+		//------------------
+		
+//		//Se busca un elemento por ID.
 //		IdentifierLoadAccess identifier = session.byId(Maestro.class); 	
 //		Maestro objMaestroLoad = (Maestro) identifier.load(15);
 //		System.out.println(objMaestroLoad);
 		
 		
-//		CONSULTAS CON HQL
+//		//***********HQL***************
+//		//Se crea la consulta. 
 //		@SuppressWarnings("unchecked")
 //		Query<Maestro> query = session.createQuery("from Maestro where nombre = :nombre");
+//		
+//		//Se agregan los parámetros de la consulta. 
 //		query.setParameter("nombre", "diego");
+//		
+//		//Se obtienen los resultados de la consulta. 
 //		List<Maestro> maestros = query.getResultList();
+//		
+//		//Se muestran los resultados de la consulta y  el tamaño de la lista. 
 //		System.out.println(maestros.size());
 //		for (Maestro maestro : maestros) {
 //			System.out.println(maestro);
 //		}
 
 		
-		
-		CriteriaBuilder builder = session.getCriteriaBuilder();
-		CriteriaQuery<Maestro> criteria = builder.createQuery(Maestro.class);
-		
-		Root<Maestro> root  = criteria.from(Maestro.class);
-		
-		criteria.select(root);
-		
-		List<Maestro> maestros = session.createQuery(criteria).getResultList();
-		for (Maestro maestro : maestros) {
-			System.out.println(maestro);
-		}
+//		//********CRITERIA**************
+//		//Se crea el manejador de criterios. 
+//		CriteriaBuilder builder = session.getCriteriaBuilder();
+//		
+//		//Se espesifica la clase mapeada para la consulta con criterios. 
+//		CriteriaQuery<Maestro> criteria = builder.createQuery(Maestro.class);
+//		
+//		//Se crea una raíz para la consulta. 
+//		Root<Maestro> root  = criteria.from(Maestro.class);
+//		
+//		//Se carga un select al criterio. 
+//		criteria.select(root);
+//		
+//		//Se ejecuta el query con criterios y se obtiene el resultado. 
+//		List<Maestro> maestros = session.createQuery(criteria).getResultList();
+//		
+//		//Se muestra la información resultante. 
+//		for (Maestro maestro : maestros) {
+//			System.out.println(maestro);
+//		}
 		
 		
 		//session.getTransaction().rollback();
